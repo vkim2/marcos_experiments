0248f2871b15126ce4f6e698b6630bbaea5f4127
vkim02 <vkim02@vt.edu>
2026-09-21 12:27:13 -0400

    add my first experiment

---
+++ my_first_experiment.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+
+st = pdb.set_trace
+
+
+import external  # imports external.py
+import experiment as ex  #
+
+
+def my_first_experiment():
+    exp = ex.Experiment(lo_freq=5, rx_t=3.125)
+
+
+if __name__ == "__main__":
+    my_first_experiment()
